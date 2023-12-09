326f850b66e0396b134d613a4fc60626838ee454
bhushank <bhushank.bwd@gmail.com>
2023-12-09 19:33:01 +0530

    added JSX

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ const headings = React.createElement(
   },
   "Hello new heading Parcel"
 );
-const headings2 = React.createElement(
-  "h1",
-  {
-    key: "2",
-    style: { color: "green" },
-  },
-  "Hello new heading 2"
-);
-const divC = React.createElement(
-  "div",
-  {
-    id: "container",
-    style: { color: "green" },
-  },
-  [headings, headings2]
+const headings2 = <h1 key={2}>Second heading</h1>;
+const Headings3 = () => <h2 key={3}>Third heading</h2>;
+const heading4 = () => {
+  return <h4 key={3}>Fourth heading</h4>;
+};
+const DivC = () => (
+  <>
+    <div>
+      {headings}
+      {headings2}
+      {Headings3()}
+      <Headings3 />
+      <Headings4 />
+    </div>
+  </>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing react element in root
-root.render(divC);
+// root.render(divC);
+root.render(<DivC />);
