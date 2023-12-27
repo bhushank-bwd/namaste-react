b91e1926d5cfd63298e0f66025dbfe21b8f42bd2
bhushank <bhushank.bwd@gmail.com>
2023-12-27 19:55:26 +0530

    jest tobeIndocumnt test done

--- src/components/__tests__/sum.test.js
+++ src/components/__tests__/sum.test.js
@@ -3,5 +3,5 @@ import { sum } from "../sum";
 test("test sum", () => {
   // first arg is description of test
   const result = sum(3, 4);
-  expect(result).toBe(5); // this is called assertion; test with different case
+  expect(result).toBe(7); // this is called assertion; test with different case
 });
