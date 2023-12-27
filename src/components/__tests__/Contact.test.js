097a310dab4ed3e8f2b9bb883f533421965e9ff3
bhushank <bhushank.bwd@gmail.com>
2023-12-27 20:10:23 +0530

    react test cases added on Contac.jsx

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -4,6 +4,18 @@ import "@testing-library/jest-dom";
 
 test("test contact header", () => {
   render(<Contact />);
-  const heading = screen.getByRole("heading");
+  const heading = screen.getByRole("heading"); // use another keyword to find instead of heading// this quering
   expect(heading).toBeInTheDocument();
+});
+test("test text", () => {
+  render(<Contact />);
+  const submit = screen.getByText("Submit"); // check all get suggestion
+  expect(submit).toBeInTheDocument();
+});
+test("test multiple", () => {
+  render(<Contact />);
+  const textbox = screen.getAllByRole("textbox"); // check all get suggestion
+  //   console.log(textbox);
+  //   console.log(textbox.length);
+  expect(textbox.length).toBe(2);
 });
